927b65f767330aa829ace4e2f61b26279e730066
Weinilin <weinilinhappy@gmail.com>
2015-10-10 20:58:57 +0800

    update controller

---
+++ src/Analyser/Analyser.java
@@ -0,0 +1,14 @@
+package Analyser;
+import java.util.ArrayList;
+import com.parser.*;
+
+public class Analyser {
+
+	public Analyser(){
+		
+	}
+	
+	public ArrayList<ArrayList<String>> analyse(JobDescObject jobDesc, ArrayList<CVObject> cvs){
+		
+	}
+}
