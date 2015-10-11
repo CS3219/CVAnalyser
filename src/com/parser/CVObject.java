008cd2f3972f60358e372538931f43cf1da2dddd
GVishnuPriya <vishki.priya@gmail.com>
2015-10-11 16:56:35 +0800

    CVObject class done

--- src/com/parser/CVObject.java
+++ src/com/parser/CVObject.java
@@ -1,5 +1,36 @@
 package com.parser;
-
+import java.util.ArrayList;
 public class CVObject {
-
+	private String name;
+	private ArrayList<String> education;
+	private ArrayList<String> skills;
+	
+	public CVObject() {
+		
+	}
+	
+	public void setName(String name) {
+		this.name = name; 
+	}
+	
+	public void setEducation(ArrayList<String> education) {
+		this.education = education;
+	}
+	
+	public void setSkills(ArrayList<String> skills) {
+		this.skills = skills;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public ArrayList<String> getEducation() {
+		return education;
+	}
+	
+	public ArrayList<String> getSkills() {
+		return skills;
+	}
+	
 }
