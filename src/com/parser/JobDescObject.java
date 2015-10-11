402756e548cdcbbed21bd2002f3561f317dce682
GVishnuPriya <vishki.priya@gmail.com>
2015-10-11 16:44:01 +0800

    Added necessary classes

---
+++ src/com/parser/JobDescObject.java
@@ -0,0 +1,33 @@
+package com.parser;
+import java.util.ArrayList;
+
+public class JobDescObject {
+	private ArrayList<String >description;
+	private ArrayList<String> responsibilites;
+	private ArrayList<String> bonusQ;
+	private String residentialStatus;
+	
+	public JobDescObject() {
+		
+	}
+	
+	public ArrayList<String> getDesc() {
+		
+	}
+	
+	public ArrayList<String> getResponsibilities() {
+		
+	}
+	
+	public ArrayList<String> getRequirements() {
+		
+	}
+	
+	public ArrayList<String> getBonusQualifications() {
+		
+	}
+	
+	public ArrayList<String> getResidentialStatus() {
+		
+	}
+}
---
+++ src/com/parser/SectionsParser.java
@@ -0,0 +1,5 @@
+package com.parser;
+
+public class SectionsParser {
+
+}
