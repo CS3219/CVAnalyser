0a167db3cbdf7c0e710cc3bd372e50e0212b45b9
ManikaAgarwal <manika615@gmail.com>
2015-10-12 18:22:35 +0800

    changed fn signature

--- src/com/parser/JobDescParser.java
+++ src/com/parser/JobDescParser.java
@@ -1,11 +1,25 @@
 package com.parser;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class JobDescParser {
 	
-    JobDescObject parseJobDesc(String position, ArrayList<String> eduReq, 
-            ArrayList<String> techSkills) {
-        return new JobDescObject(position, eduReq, techSkills);
+    JobDescObject parseJobDesc(String position, String eduReq, String techSkills) {
+        StringTokenizer st = new StringTokenizer(eduReq, "\n");
+        ArrayList<String> edu = new ArrayList<String>();
+        
+        while(st.hasMoreTokens()) {
+            edu.add(st.nextToken());
+        }
+        
+        st = new StringTokenizer(techSkills, "\n,");
+        ArrayList<String> tech = new ArrayList<String>();
+        
+        while(st.hasMoreTokens()) {
+            tech.add(st.nextToken());
+        }
+        
+        return new JobDescObject(position, edu, tech);
     }
 	
 }
