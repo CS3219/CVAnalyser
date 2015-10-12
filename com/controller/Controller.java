d15e0e241641c3daf3a1a6bd535fb3e6aafe7492
gangadevi <gangadevi.balakrishnan@gmail.com>
2015-10-12 16:45:02 +0800

    Update controller.

--- com/controller/Controller.java
+++ com/controller/Controller.java
@@ -12,7 +12,7 @@ ArrayList<CVObject> cvs = new ArrayList<CVObject>();
 		
 	}
 	
-	public ArrayList<ResultDetail> processJobDescAndCV(String jobDesc, ArrayList<String> CV){
+	public ArrayList<ArrayList<String>> processJobDescAndCV(String position,String eduReq,String techSkills, ArrayList<String> CV){
 		CVParser cvParser = new CVParser();
 		JobDescParser jobDescParser = new JobDescParser();
 		Analyser analyser = new Analyser();
@@ -20,9 +20,9 @@ ArrayList<CVObject> cvs = new ArrayList<CVObject>();
 		for(int i = 0; i < CV.size(); i++) {
 			cvs.add(cvParser.parseCV(CV.get(i)));
 		}
-		JobDescObject jobDescriptions = jobDescParser.parseJobDesc(jobDesc);
 		
-		ArrayList<ResultDetail> results = analyser.analyse(jobDescriptions, cvs);
+		JobDescObject jobDescriptions = jobDescParser.parseJobDesc(position,eduReq,techSkills);
+		ArrayList<ArrayList<String>> results = analyser.analyse(jobDescriptions, cvs);
 		
 		return results;
 	}
