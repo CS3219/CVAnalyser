ab99a12df46447231bde52163bd76b81207c99a2
GVishnuPriya <vishki.priya@gmail.com>
2015-10-30 01:15:00 +0800

    added classes

--- src/com/parser/JobDescObject.java
+++ src/com/parser/JobDescObject.java
@@ -4,36 +4,75 @@ import java.util.ArrayList;
 
 public class JobDescObject {
 
-	private ArrayList<String> description;
-	private ArrayList<String> responsibilites;
-	private ArrayList<String> bonusQ;
-	private String residentialStatus;
-	private String position;
-	private ArrayList<String> eduReq;
-	private ArrayList<String> techSkillReq;
+	private ArrayList<String> responsibilities;
+	private ArrayList<String>minSkills;
+	//private String residentialStatus;
+	private ArrayList<String> minEdu;
+	private ArrayList<workExp> minWorkExp;
+	private ArrayList<String>bonusSkills;
+	private ArrayList<String>bonusEdu;
+	private ArrayList<workExp>bonusWorkExp;
 
-	public JobDescObject(String position, ArrayList<String> eduReq, ArrayList<String> techSkillReq) {
+	public JobDescObject() {
 
-		this.position = position;
-		this.eduReq = eduReq;
-		this.techSkillReq = techSkillReq;
 	}
 
-	public String getPosition() {
-		return position;
+	public ArrayList<String> getResponsibilites() {
+		return responsibilities;
 	}
 
-	public ArrayList<String> getEduReq() {
-		SectionParser sectionParser = new SectionParser();
-		eduReq = sectionParser.parseEducation(eduReq);
+	public ArrayList<String> getMinSkills() {
 
-		return eduReq;
+		return minSkills;
+	}
+	
+	public ArrayList<String> getMinEdu() {
+		return minEdu;
+	}
+	
+	public ArrayList<workExp> getMinWorkExp() {
+		return minWorkExp;
 	}
 
-	public ArrayList<String> getTechSkillReq() {
-		SectionParser sectionParser = new SectionParser();
-		techSkillReq = sectionParser.parseSkills(techSkillReq);
-		return techSkillReq;
+	public ArrayList<String> getBonusSkills() {
+		return bonusSkills;
+	}
+	
+	public ArrayList<String> getBonusEdu() {
+		return bonusEdu;
+	}
+	
+	public ArrayList<workExp> getBonusWorkExp() {
+	return bonusWorkExp;	
+		
+	}
+	
+	public void  setResponsibilites(ArrayList<String> responsibilities) {
+		this.responsibilities = responsibilities;
 	}
 
+	public void setMinSkills(ArrayList<String> minSkills) {
+		this.minSkills = minSkills;
+	}
+	
+	public void setMinEdu(ArrayList<String> minEdu) {
+		this.minEdu = minEdu;
+	}
+	
+	public void setMinWorkExp(ArrayList<workExp> minWorkExp) {
+		this.minWorkExp = minWorkExp;
+	}
+	
+	public void setBonusSkills(ArrayList<String> bonusSkills) {
+		this.bonusSkills = bonusSkills;
+	}
+	
+	public void setBonusEdu(ArrayList<String> bonusEdu) {
+		this.bonusEdu = bonusEdu;
+	}
+	
+	public void setBonusWorkExp(ArrayList<workExp> bonusWorkExp) {
+		this.bonusWorkExp = bonusWorkExp;
+	}
+	
 }
--- src/com/parser/JobDescParser.java
+++ src/com/parser/JobDescParser.java
@@ -1,29 +1,92 @@
 package com.parser;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.StringTokenizer;
+
+import com.parser.Annotation;
+import com.parser.CoreLabel;
+import com.parser.CoreMap;
+import stanfordnlp.LemmaAnnotation;
+import stanfordnlp.SentencesAnnotation;
+import stanfordnlp.StanfordCoreNLP;
+import stanfordnlp.TextAnnotation;
+import stanfordnlp.TokensAnnotation;
 
 public class JobDescParser {
 	
+	private static final ArrayList<String> Headers = new ArrayList<String>();
+	private static JobDescObject jobDescObject = new JobDescObject();
+	private ArrayList<Integer> headerLines = new ArrayList<Integer>();
+	
 	public JobDescParser() {
+		Headers.add(0, "Responsibilities");
+		Headers.add(1,"Minimum Requirements");
+		Headers.add(2, "Preferred Qualifications");
 		
 	}
 	
-    public JobDescObject parseJobDesc(String position, String eduReq, String techSkills) {
-        StringTokenizer st = new StringTokenizer(eduReq, "\n");
-        ArrayList<String> edu = new ArrayList<String>();
-        
-        while(st.hasMoreTokens()) {
-            edu.add(st.nextToken());
-        }
-        
-        st = new StringTokenizer(techSkills, "\n.");
-        ArrayList<String> tech = new ArrayList<String>();
-        
-        while(st.hasMoreTokens()) {
-            tech.add(st.nextToken());
-        }
-        
-        return new JobDescObject(position, edu, tech);
-    }
+	public JobDescObject parseJobDesc(String fileName) {
+		//extract the lines from file
+				ArrayList<String> lines = extractLines(fileName);
+				sectionsExtractor Se = new sectionsExtractor();
+				headerLines = Se.extractSections(lines);
+				for( int i=0 ;i<lines.size(); i++) {
+					String line = lines.get(i);
+					lines.set(i, line.toLowerCase());
+				}
+				ArrayList<String > updatedLines = parseNlp(lines);
+				ArrayList<String> responsibilities = (ArrayList<String>) updatedLines.subList(headerLines.get(0), headerLines.get(1)- 1);
+				ArrayList<String> minReq = (ArrayList<String>) updatedLines.subList(headerLines.get(1), headerLines.get(2) - 1);
+				ArrayList<String> bonusQualifications = (ArrayList<String>) updatedLines.subList(headerLines.get(2), headerLines.size()-1);
+parseResponsibilities(responsibilities);
+parseMinReq(minReq);
+parseBonus(bonusQualifications);
+		return jobDescObject;
+	}
 	
+	private void parseResponsibilities(ArrayList<String> responsibilities) {
+		
+	
+	}
+	
+	private void parseMinReq(ArrayList<String> minReq) {
+		for(int i=0;i<minReq.size();i++) {
+			String line = minReq.get(i);
+			
+		}
+	}
+	
+
+	private void parseBonus(ArrayList<String> bonus) {
+		
+	}
+	
+	private ArrayList<String> parseNlp(ArrayList<String> lines) {
+		Properties props = new Properties(); 
+	    props.put("annotators", "tokenize, ssplit, pos, lemma"); 
+	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props, false);
+	    
+	    for(int i=0;i<lines.size();i++) {
+	    String line = lines.get(i); 
+	    Annotation document = pipeline.process(line);  
+
+	    for(CoreMap sentence: document.get(SentencesAnnotation.class))
+	    {    
+	        for(CoreLabel token: sentence.get(TokensAnnotation.class))
+	        {       
+	            String word = token.get(TextAnnotation.class);      
+	            String lemma = token.get(LemmaAnnotation.class); 
+	            System.out.println("lemmatized version :" + lemma);
+	            lines.set(i, lemma);
+	        }
+	    }
+	    }
+	    
+	    return lines;
+	}
+   private ArrayList<String> extractLines(String fileName) {
+	  
+   }
+   
+  	
 }
